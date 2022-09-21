5341a47f3744a721b56404978c61a351bfb6bf9a
Alain Schaller <schallerala@gmail.com>
2022-09-21 21:25:38 +0200

    Adapt config to avoid out of memory on the GPUs

--- src/ilids/towhee_utils/override/movinet_config.py
+++ src/ilids/towhee_utils/override/movinet_config.py
@@ -3,14 +3,15 @@ from typing import Dict
 
 logger = logging.getLogger(__name__)
 
-# Taken from: https://github.com/Atze00/MoViNet-pytorch
+# Inspired from: https://github.com/Atze00/MoViNet-pytorch
+# But also: https://github.com/tensorflow/models/tree/master/official/projects/movinet/configs/yaml
 _movinet_transform_cfgs = dict(
     movineta0=dict(side_size=172, crop_size=172, num_frames=50),
     movineta1=dict(side_size=172, crop_size=172, num_frames=50),
     movineta2=dict(side_size=224, crop_size=224, num_frames=50),
-    movineta3=dict(side_size=256, crop_size=256, num_frames=120),
-    movineta4=dict(side_size=290, crop_size=290, num_frames=80),
-    movineta5=dict(side_size=320, crop_size=320, num_frames=120),
+    movineta3=dict(side_size=224, crop_size=224, num_frames=80),
+    movineta4=dict(side_size=224, crop_size=224, num_frames=50),
+    movineta5=dict(side_size=224, crop_size=224, num_frames=25),
 )
 
 
