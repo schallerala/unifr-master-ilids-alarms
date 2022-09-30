38e977e2606a82574933ea63d8b5c97ae4cf691d
Alain Schaller <schallerala@gmail.com>
2022-09-30 10:01:55 +0200

    Dataset module should be ready for experiment

--- src/ilids/models/actionclip/datasets/datasets.py
+++ src/ilids/models/actionclip/datasets/datasets.py
@@ -1,159 +1,59 @@
 # Code for "ActionCLIP: ActionCLIP: A New Paradigm for Action Recognition"
 # arXiv:
 # Mengmeng Wang, Jiazheng Xing, Yong Liu
+from pathlib import Path
 
-import os
-import os.path
-
+import PIL
 import numpy as np
 import pandas as pd
-import torch
 import torch.utils.data as data
+from decord import VideoReader, cpu
 from numpy.random import randint
 from PIL import Image
 
 
-class GroupTransform(object):
-    def __init__(self, transform):
-        self.worker = transform
-
-    def __call__(self, img_group):
-        return [self.worker(img) for img in img_group]
-
-
-class ToTorchFormatTensor(object):
-    """Converts a PIL.Image (RGB) or numpy.ndarray (H x W x C) in the range [0, 255]
-    to a torch.FloatTensor of shape (C x H x W) in the range [0.0, 1.0]"""
-
-    def __init__(self, div=True):
-        self.div = div
-
-    def __call__(self, pic):
-        if isinstance(pic, np.ndarray):
-            img = torch.from_numpy(pic).permute(2, 0, 1).contiguous()
-        else:
-            img = torch.ByteTensor(torch.ByteStorage.from_buffer(pic.tobytes()))
-            img = img.view(pic.size[1], pic.size[0], len(pic.mode))
-            img = img.transpose(0, 1).transpose(0, 2).contiguous()
-        return img.float().div(255) if self.div else img.float()
-
-
-class Stack(object):
-    def __init__(self, roll=False):
-        self.roll = roll
-
-    def __call__(self, img_group):
-        if img_group[0].mode == "L":
-            return np.concatenate([np.expand_dims(x, 2) for x in img_group], axis=2)
-        elif img_group[0].mode == "RGB":
-            if self.roll:
-                print(len(img_group))
-                return np.concatenate(
-                    [np.array(x)[:, :, ::-1] for x in img_group], axis=2
-                )
-            else:
-                print(len(img_group))
-                rst = np.concatenate(img_group, axis=2)
-                return rst
-
-
-class VideoRecord(object):
-    def __init__(self, row):
-        self._data = row
-
-    @property
-    def path(self):
-        return self._data[0]
-
-    @property
-    def num_frames(self):
-        return int(self._data[1])
-
-    @property
-    def label(self):
-        return int(self._data[2])
-
-
-class Action_DATASETS(data.Dataset):
+class ActionDataset(data.Dataset):
     def __init__(
         self,
-        list_file,
-        labels_file,
-        num_segments=1,
-        new_length=1,
-        image_tmpl="img_{:05d}.jpg",
+        sequences_details_file: Path,
+        frames_to_extract: int = 8,
+        seg_length: int = 1,  # TODO not sure what it does
         transform=None,
         random_shift=True,
-        test_mode=False,
-        index_bias=1,
+        index_bias: int = 1,
     ):
-
-        self.list_file = list_file
-        self.num_segments = num_segments  # 8
-        self.seg_length = new_length  # 1
-        self.image_tmpl = image_tmpl
+        self.frames_to_extract = frames_to_extract  # 8
+        self.seg_length = seg_length  # 1
         self.transform = transform
         self.random_shift = random_shift
-        self.test_mode = test_mode
-        self.loop = False
         self.index_bias = index_bias
-        self.labels_file = labels_file
 
-        if self.index_bias is None:
-            if self.image_tmpl == "frame{:d}.jpg":
-                self.index_bias = 0
-            else:
-                self.index_bias = 1
-        self._parse_list()
-        self.initialized = False
-
-    def _load_image(self, directory, idx):
-        return [
-            Image.open(os.path.join(directory, self.image_tmpl.format(idx))).convert(
-                "RGB"
-            )
-        ]
+        self._sequences_df = pd.read_csv(sequences_details_file)
+        # require at least those 2 columns
+        assert len(self._sequences_df.columns.intersection(pd.Index("sequence", "frame_count"))) == 2
 
     @property
-    def total_length(self) -> int:  # always returns 8 * 1 = 8
-        return self.num_segments * self.seg_length
+    def _total_length(self) -> int:
+        return self.frames_to_extract * self.seg_length
 
-    @property
-    def classes(self):
-        classes_all = pd.read_csv(self.labels_file)
-        return classes_all.values.tolist()
-
-    def _parse_list(self):
-        self.video_list = [
-            VideoRecord(x.strip().split(" ")) for x in open(self.list_file)
-        ]
-
-    def _sample_indices(self, record):
-        if record.num_frames <= self.total_length:
-            if self.loop:
-                return (
-                    np.mod(
-                        np.arange(self.total_length) + randint(record.num_frames // 2),
-                        record.num_frames,
-                    )
-                    + self.index_bias
-                )
+    def _random_sample_indices(self, record_num_frames: int) -> np.ndarray:
+        if record_num_frames <= self._total_length:
             offsets = np.concatenate(
                 (
-                    np.arange(record.num_frames),
+                    np.arange(record_num_frames),
                     randint(
-                        record.num_frames, size=self.total_length - record.num_frames
+                        record_num_frames, size=self._total_length - record_num_frames
                     ),
                 )
             )
             return np.sort(offsets) + self.index_bias
         offsets = list()
         ticks = [
-            i * record.num_frames // self.num_segments
-            for i in range(self.num_segments + 1)
+            i * record_num_frames // self.frames_to_extract
+            for i in range(self.frames_to_extract + 1)
         ]
 
-        for i in range(self.num_segments):
+        for i in range(self.frames_to_extract):
             tick_len = ticks[i + 1] - ticks[i]
             tick = ticks[i]
             if tick_len >= self.seg_length:
@@ -161,32 +61,27 @@ class Action_DATASETS(data.Dataset):
             offsets.extend([j for j in range(tick, tick + self.seg_length)])
         return np.array(offsets) + self.index_bias
 
-    def _get_val_indices(self, record):
-        if self.num_segments == 1:
-            return np.array([record.num_frames // 2], dtype=np.int) + self.index_bias
+    def _sample_indices(self, record_num_frames: int) -> np.ndarray:
+        if self.frames_to_extract == 1:
+            return np.array([record_num_frames // 2], dtype=np.int) + self.index_bias
 
-        if record.num_frames <= self.total_length:
-            if self.loop:
-                return (
-                    np.mod(np.arange(self.total_length), record.num_frames)
-                    + self.index_bias
-                )
+        if record_num_frames <= self._total_length:
             return (
                 np.array(
                     [
-                        i * record.num_frames // self.total_length
-                        for i in range(self.total_length)
+                        i * record_num_frames // self._total_length
+                        for i in range(self._total_length)
                     ],
                     dtype=np.int,
                 )
                 + self.index_bias
             )
-        offset = (record.num_frames / self.num_segments - self.seg_length) / 2.0
+        offset = (record_num_frames / self.frames_to_extract - self.seg_length) / 2.0
         return (
             np.array(
                 [
-                    i * record.num_frames / self.num_segments + offset + j
-                    for i in range(self.num_segments)
+                    i * record_num_frames / self.frames_to_extract + offset + j
+                    for i in range(self.frames_to_extract)
                     for j in range(self.seg_length)
                 ],
                 dtype=np.int,
@@ -195,30 +90,28 @@ class Action_DATASETS(data.Dataset):
         )
 
     def __getitem__(self, index):
-        record = self.video_list[index]
-        segment_indices = (
-            self._sample_indices(record)
+        frame_count = self._sequences_df[index, "frame_count"]
+        sequence_frame_indices = (
+            self._random_sample_indices(frame_count)
             if self.random_shift
-            else self._get_val_indices(record)
+            else self._sample_indices(frame_count)
         )
-        return self.get(record, segment_indices)
+        return self.get(self._sequences_df[index, "sequence"], sequence_frame_indices)
 
     def __call__(self, img_group):
         return [self.worker(img) for img in img_group]
 
-    def get(self, record, indices):
-        images = list()
-        for i, seg_ind in enumerate(indices):
-            p = int(seg_ind)
-            try:
-                seg_imgs = self._load_image(record.path, p)
-            except OSError:
-                print('ERROR: Could not read image "{}"'.format(record.path))
-                print("invalid indices: {}".format(indices))
-                raise
-            images.extend(seg_imgs)
-        process_data = self.transform(images)
-        return process_data, record.label
+    def get(self, sequence_path: str, sequence_frame_indices: np.ndarray):
+        """Extract the frames for the record, transform them as PIL.Images and transform them with
+        `self.transform`"""
+        vr = VideoReader(sequence_path, ctx=cpu(0))
+        frames = vr.get_batch(sequence_frame_indices)
+
+        pil_frames = [PIL.Image.fromarray(frame) for frame in frames.asnumpy()]
+
+        process_data = self.transform(pil_frames)
+
+        return process_data
 
     def __len__(self):
-        return len(self.video_list)
+        return len(self._sequences_df)
