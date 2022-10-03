b380a0bbc4b35c28fb49708163c5db00ede20c85
Alain Schaller <schallerala@gmail.com>
2022-10-03 14:01:24 +0200

    Avoid indentation

--- src/ilids/commands/experiments.py
+++ src/ilids/commands/experiments.py
@@ -116,37 +116,36 @@ def actionclip(
     assert features_output_path.parent.exists()
     assert features_output_path.parent.is_dir()
 
-    with notify_context(enable=notify):
+    with notify_context(enable=notify), alternate_device(
+        device_type, distributed, sync_server_host, sync_server_port
+    ) as device:
         print(f"Starting features extract for {model_name}...")
 
-        with alternate_device(
-            device_type, distributed, sync_server_host, sync_server_port
-        ) as device:
-            (
-                model_image,
-                model_text,
-                fusion_model,
-                preprocess_image,
-            ) = create_models_and_transforms(
-                actionclip_pretrained_ckpt=model_pretrained_checkpoint,
-                openai_model_name=model_name,
-            )
+        (
+            model_image,
+            model_text,
+            fusion_model,
+            preprocess_image,
+        ) = create_models_and_transforms(
+            actionclip_pretrained_ckpt=model_pretrained_checkpoint,
+            openai_model_name=model_name,
+        )
 
-            ilids_dataset = ActionDataset(
-                list_input_sequences_file, transform=get_augmentation()
-            )
-            loader_num_workers = loader_num_workers or cpu_count()
-            ilids_loader = DataLoader(
-                ilids_dataset,
-                batch_size=batch_size,
-                num_workers=loader_num_workers,
-                shuffle=False,
-                pin_memory=True,
-            )
+        ilids_dataset = ActionDataset(
+            list_input_sequences_file, transform=get_augmentation()
+        )
+        loader_num_workers = loader_num_workers or cpu_count()
+        ilids_loader = DataLoader(
+            ilids_dataset,
+            batch_size=batch_size,
+            num_workers=loader_num_workers,
+            shuffle=False,
+            pin_memory=True,
+        )
 
-            features_df = extract_actionclip_sequences_features(
-                model_image, fusion_model, ilids_loader, device
-            )
+        features_df = extract_actionclip_sequences_features(
+            model_image, fusion_model, ilids_loader, device
+        )
 
         PersistenceMethod.get_from_extension(features_output_path).get_persistence_impl(
             PersistenceMethodSource.pandas
