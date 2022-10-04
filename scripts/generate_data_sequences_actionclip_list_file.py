26897ef3e4886ec4f8bfc5bd33411c68deb489ca
Alain Schaller <schallerala@gmail.com>
2022-10-04 11:31:28 +0200

    Fix wrong frame count of sequences
    Due to the logic of always selecting the last frame in the frame extraction process

--- scripts/generate_data_sequences_actionclip_list_file.py
+++ scripts/generate_data_sequences_actionclip_list_file.py
@@ -12,7 +12,7 @@ def main(
     output: Path,
     initial_fps: int,
     extracted_sequences_folder: Path,
-    extracted_frame_stride: int,
+    frame_stride: int,
     new_fps: Optional[int] = typer.Option(None),
     overwrite: bool = typer.Option(False, "--force", "-f"),
 ):
@@ -20,7 +20,7 @@ def main(
     assert tp_fp_sequences_file.is_file()
 
     assert output.parent.exists()
-    assert overwrite or not output.exists()
+    # assert overwrite or not output.exists()
 
     if not new_fps:
         new_fps = initial_fps
@@ -29,18 +29,27 @@ def main(
 
     df["Duration"] = pd.to_timedelta(df["Duration"])
 
-    # To compute the number of frames: math.ceil((initial_duration * fps) / extracted_frames + 1)
+    # To compute the number of frames:
+    #   if (initial_duration * fps - 1) % frame_stride == 0:
+    #       math.ceil((initial_duration * fps) / frame_stride)
+    #   else:
+    #       math.ceil((initial_duration * fps) / frame_stride + 1)
     file_paths = str(extracted_sequences_folder).rstrip("/") + "/" + df.index
-    frames_count = (
-        df["Duration"].dt.seconds * initial_fps / extracted_frame_stride + 1
-    ).apply(np.ceil).astype('int64')
+    total_frames = df["Duration"].dt.seconds * initial_fps
+    nb_frames = total_frames / frame_stride
+
+    missing_last_frame = (total_frames - 1) % frame_stride > 0
+
+    nb_frames[missing_last_frame] += 1
+
+    nb_frames = nb_frames.apply(np.ceil).astype('int64')
     # To have its time in seconds, same formula as above but lastly divided by the new fps (here same)
-    new_duration = (frames_count / new_fps).apply(
+    new_duration = (nb_frames / new_fps).apply(
         lambda secs: strftime("%H:%M:%S", gmtime(secs))
     )
 
     new_df = pd.DataFrame(
-        np.array([file_paths, frames_count, new_duration]).T,
+        np.array([file_paths, nb_frames, new_duration]).T,
         columns=["sequence", "frame_count", "duration"],
     ).set_index("sequence")
 
--- tests/commands/videos/test_frames_extraction.py
+++ tests/commands/videos/test_frames_extraction.py
@@ -10,6 +10,32 @@ from ilids.commands.videos.frames_extraction import typer_app
 runner = CliRunner()
 
 
+@pytest.mark.szte_files((Path("video") / "SZTEA101a.mov", "input_sequence"))
+@pytest.mark.parametrize("start_time,end_time,output_nb_frames", [("00:27:58", "00:28:59", 128), ("00:17:01", "00:17:48", 99)])
+def test_extract__SZTEA101a_issue_frame_count(input_sequence: Path, tmp_path: Path, start_time: str, end_time: str, output_nb_frames: int):
+    output_file = tmp_path / "output.mov"
+
+    result = runner.invoke(
+        typer_app,
+        [str(input_sequence), start_time, end_time, "12", "-o", str(output_file)],
+    )
+
+    assert result.exit_code == 0
+
+    assert output_file.exists()
+
+    output_info = get_stream_info(output_file)
+
+    assert len(output_info.streams) == 1
+
+    assert_that(output_info.streams[0].avg_fps, is_(close_to(25, 0.1)))
+    assert_that(output_info.streams[0].height, is_(224))
+    assert_that(output_info.streams[0].width, is_(280))
+    assert_that(output_info.streams[0].nb_frames, is_(output_nb_frames))
+    assert_that(output_info.format.start_time, is_(close_to(0, 0.00001)))
+    assert_that(output_info.format.duration, is_(close_to(output_nb_frames / 25, 0.01)))
+
+
 def test_extract(create_sample_video, tmp_path: Path):
     # create a video of 2 minutes
     input_video_path = create_sample_video(120, (720, 576), 25, ".mov")
